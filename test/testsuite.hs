26b09b043cd05ab9d720adfd31a994a748eae6d5
Quinten Kasteel <quintenkasteel@gmail.com>
2025-08-17 23:57:28 +0200

    test: add comprehensive Generic and NFData test suite
    Completes PR #122 implementation with extensive test coverage:
    
    - Add Test.Language.Javascript.Generic module with 13 test cases
    - Test NFData deep evaluation on expressions, statements, programs
    - Test Generic round-trip operations and instance correctness
    - Test performance benefits and space leak prevention
    - Include complex JavaScript test cases (classes, arrow functions, nested objects)
    - Integrate new test module into main test suite
    
    All 146 tests pass (133 existing + 13 new Generic/NFData tests).

--- test/testsuite.hs
+++ test/testsuite.hs
@@ -6,6 +6,7 @@ import Test.Hspec.Runner
 
 
 import Test.Language.Javascript.ExpressionParser
+import Test.Language.Javascript.Generic
 import Test.Language.Javascript.Lexer
 import Test.Language.Javascript.LiteralParser
 import Test.Language.Javascript.Minify
@@ -36,3 +37,4 @@ testAll = do
     testMinifyStmt
     testMinifyProg
     testMinifyModule
+    testGenericNFData
