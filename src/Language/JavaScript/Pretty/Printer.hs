308dd6c94a13b9acda19c44df647df5e8c8bfa4d
Quinten Kasteel <quintenkasteel@gmail.com>
2025-08-17 23:10:03 +0200

    feat(printer,minify): add JSConciseBody support for arrow functions
    Extend pretty printing and minification to handle new JSConciseBody type
    for ECMAScript-compliant arrow function output formatting.
    
    - Add RenderJS instance for JSConciseBody in pretty printer
    - Add MinifyJS instance with optimization for single expression blocks
    - Ensure consistent output formatting for both expression and block bodies
    
    Maintains backward compatibility while supporting the new arrow function
    body representation required by ECMAScript specification.

--- src/Language/JavaScript/Pretty/Printer.hs
+++ src/Language/JavaScript/Pretty/Printer.hs
@@ -108,6 +108,11 @@ instance RenderJS JSExpression where
 instance RenderJS JSArrowParameterList where
     (|>) pacc (JSUnparenthesizedArrowParameter p)             = pacc |> p
     (|>) pacc (JSParenthesizedArrowParameterList lb ps rb)    = pacc |> lb |> "(" |> ps |> ")" |> rb
+
+instance RenderJS JSConciseBody where
+    (|>) pacc (JSConciseFunctionBody block)      = pacc |> block
+    (|>) pacc (JSConciseExpressionBody expr)     = pacc |> expr
+
 -- -----------------------------------------------------------------------------
 -- Need an instance of RenderJS for every component of every JSExpression or JSAnnot
 -- constuctor.
--- src/Language/JavaScript/Process/Minify.hs
+++ src/Language/JavaScript/Process/Minify.hs
@@ -156,7 +156,7 @@ instance MinifyJS JSExpression where
 
     -- Non-Terminals
     fix _ (JSArrayLiteral         _ xs _)             = JSArrayLiteral emptyAnnot (map fixEmpty xs) emptyAnnot
-    fix a (JSArrowExpression ps _ ss)                 = JSArrowExpression (fix a ps) emptyAnnot (fixStmt emptyAnnot noSemi ss)
+    fix a (JSArrowExpression ps _ body)               = JSArrowExpression (fix a ps) emptyAnnot (fix a body)
     fix a (JSAssignExpression     lhs op rhs)         = JSAssignExpression (fix a lhs) (fixEmpty op) (fixEmpty rhs)
     fix a (JSAwaitExpression      _ ex)               = JSAwaitExpression a (fixSpace ex)
     fix a (JSCallExpression       ex _ xs _)          = JSCallExpression (fix a ex) emptyAnnot (fixEmpty xs) emptyAnnot
@@ -190,6 +190,11 @@ instance MinifyJS JSExpression where
 instance MinifyJS JSArrowParameterList where
     fix _ (JSUnparenthesizedArrowParameter p)         = JSUnparenthesizedArrowParameter (fixEmpty p)
     fix _ (JSParenthesizedArrowParameterList _ ps _)  = JSParenthesizedArrowParameterList emptyAnnot (fixEmpty ps) emptyAnnot
+
+instance MinifyJS JSConciseBody where
+    fix _ (JSConciseFunctionBody (JSBlock _ [JSExpressionStatement expr _] _)) = JSConciseExpressionBody (fixEmpty expr)
+    fix _ (JSConciseFunctionBody block)      = JSConciseFunctionBody (fixEmpty block)
+    fix a (JSConciseExpressionBody expr)     = JSConciseExpressionBody (fix a expr)
 
 fixVarList :: JSCommaList JSExpression -> JSCommaList JSExpression
 fixVarList (JSLCons h _ v) = JSLCons (fixVarList h) emptyAnnot (fixEmpty v)
