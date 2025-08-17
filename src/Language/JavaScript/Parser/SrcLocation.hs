d43a0cadd5baf7284d7a65cb80190a6e1de33bc7
Quinten Kasteel <quintenkasteel@gmail.com>
2025-08-17 23:57:13 +0200

    feat(parser): add Generic and NFData to parser support types
    Extends PR #122 implementation to parser support modules:
    
    - Token.hs: Add Generic/NFData to CommentAnnotation and Token types
    - SrcLocation.hs: Add Generic/NFData to TokenPosn type
    - ParseError.hs: Add Generic/NFData to ParseError type
    
    All modules now include required language extensions and imports
    for complete Generic and NFData support across the parser.

--- src/Language/JavaScript/Parser/SrcLocation.hs
+++ src/Language/JavaScript/Parser/SrcLocation.hs
@@ -1,10 +1,12 @@
-{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE DeriveDataTypeable, DeriveGeneric, DeriveAnyClass #-}
 module Language.JavaScript.Parser.SrcLocation (
   TokenPosn(..)
   , tokenPosnEmpty
   ) where
 
+import Control.DeepSeq (NFData)
 import Data.Data
+import GHC.Generics (Generic)
 
 -- | `TokenPosn' records the location of a token in the input text.  It has three
 -- fields: the address (number of characters preceding the token), line number
@@ -14,7 +16,7 @@ import Data.Data
 data TokenPosn = TokenPn !Int -- address (number of characters preceding the token)
                          !Int -- line number
                          !Int -- column
-        deriving (Eq,Show, Read, Data, Typeable)
+        deriving (Eq, Generic, NFData, Show, Read, Data, Typeable)
 
 tokenPosnEmpty :: TokenPosn
 tokenPosnEmpty = TokenPn 0 0 0
